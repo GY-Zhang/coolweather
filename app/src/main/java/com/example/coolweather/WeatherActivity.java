bbf29db60278e75f2dce38ecc7608796e2d3a502
Mr.zhang <569844304@qq.com>
2016-12-09 18:59:23 +0800

    增加后台自动更新天气功能。

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.coolweather.gson.Weather;
+import com.example.coolweather.service.AutoUpdateService;
 import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
 
@@ -85,11 +87,9 @@ public class WeatherActivity extends AppCompatActivity {
         if (weatherString != null){
             Weather weather = Utility.handleWeatherResponse(weatherString);
             mWeatherId = weather.getHeWeather5().get(0).getBasic().getId();
-            Log.d("WeatherActivity","weatherId is "+mWeatherId);
             showWeatherInfo(weather);
         }else {
             mWeatherId = getIntent().getStringExtra("weather_id");
-            Log.d("WeatherActivity","weatherId ~~~~~~~~~~~~is "+mWeatherId);
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(mWeatherId);
         }
@@ -148,8 +148,8 @@ public class WeatherActivity extends AppCompatActivity {
             carWashText.setText(carWash);
             sportText.setText(sport);
             weatherLayout.setVisibility(View.VISIBLE);
-
-
+            Intent intent = new Intent(this, AutoUpdateService.class);
+            startService(intent);
         }
 
 
